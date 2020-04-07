f0d678c0e22e0bb306cff4346a338b475df26641
Valentinmf <40763626+Valentinmf@users.noreply.github.com>
2020-04-07 21:56:28 +0200

    Update exo_10.rb

--- Ruby-THP-J1/exo_10.rb
+++ Ruby-THP-J1/exo_10.rb
@@ -1,3 +1,4 @@
-year_of_birth = 1934
-puts "Année de naissance de donald trump #{year_of_birth}"
-puts "En 2017 Donald Trump avait #{2017 - year_of_birth} ans"
+puts "Votre année de naissance ?"
+annee_de_naissance = gets.chomp.to_i
+
+puts "en 2017 vous avez eu #{2017 - annee_de_naissance}"
